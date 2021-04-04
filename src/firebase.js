3202aa78a94aab4f55846f80b2bf0f5fc8f9c8bf
Abhiraj <abhirajkumar475@gmail.com>
2021-04-04 22:50:09 +0530

    added basic files and initialized'em

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAioOceax_Xnn2see6TAWWPoVPobuFHlmk",
+    authDomain: "project-f0468.firebaseapp.com",
+    projectId: "project-f0468",
+    storageBucket: "project-f0468.appspot.com",
+    messagingSenderId: "933802768384",
+    appId: "1:933802768384:web:f1bb0729ada7fc66f100e5",
+    measurementId: "G-8MF731449K"
+  };
